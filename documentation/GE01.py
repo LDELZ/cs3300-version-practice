507a79e4ea28ee2c96236d64766cc158560a9d5a
Luke Delzer <107493086+LDELZ@users.noreply.github.com>
2024-01-26 11:02:47 -0700

    First commit

---
+++ documentation/GE01.py
@@ -0,0 +1,6 @@
+# Student name: Luke Delzer
+# Course: Intro to Software Engineering (CS3300)
+# Assignment: GE01
+# Due Date: 01/30/2024
+print("The following python code is used to demonstrate the proper setup of Git and GitHub on my system")
+print("This change will track the first commit")
