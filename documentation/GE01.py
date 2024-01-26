46c72cb3c996df25e8a6ac020517ec1d3be698b8
Luke Delzer <107493086+LDELZ@users.noreply.github.com>
2024-01-26 11:38:03 -0700

    add description

--- documentation/GE01.py
+++ documentation/GE01.py
@@ -4,3 +4,4 @@
 # Due Date: 01/30/2024
 print("The following python code is used to demonstrate the proper setup of Git and GitHub on my system")
 print("This change will track the first commit")
+print("This line will track changes for the second commit")
